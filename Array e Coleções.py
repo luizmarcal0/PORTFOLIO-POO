498c88dbc77c12a84d653cd42c2c1923612050fa
LUIZ CARLOS MARÇAL MAURICIO <luizmauricio@ads.fiponline.edu.br>
2025-11-03 19:30:17 -0300

    3

---
+++ Array e Coleções.py
@@ -0,0 +1,15 @@
+autores_favoritos = ["J.R.R. Tolkien", "George Orwell", "Isaac Asimov"]
+autores_favoritos.append("Ursula K. Le Guin")
+print(autores_favoritos[0])
+
+coordenadas_biblioteca = (-7.22, -35.88)
+
+membro = {
+    "id": 101,
+    "nome": "Ana Silva",
+    "livros_emprestados": ["O Hobbit", "1984"]
+}
+print(membro["nome"])
+
+generos = {"Ficção", "Fantasia", "Ficção"}
+print(generos)
